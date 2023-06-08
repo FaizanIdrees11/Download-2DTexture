c1ee600aee88003a93e3e94379b6c26c1b586a92
FaizanIdrees11 <79058353+FaizanIdrees11@users.noreply.github.com>
2023-06-08 16:12:40 +0500

    Update DownloadTexture.cs

--- DownloadTexture.cs
+++ DownloadTexture.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
 
 public class DownloadTexture : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string textureUrl = "https://example.com/texture.png"; // URL of the texture to download
+
+    private void Start()
     {
-        
+        StartCoroutine(DownloadTexture());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator DownloadTexture()
     {
-        
+        using (UnityWebRequest www = UnityWebRequestTexture.GetTexture(textureUrl))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.Success)
+            {
+                // Get the downloaded texture
+                Texture2D texture = DownloadHandlerTexture.GetContent(www);
+
+                // Save the texture as a file
+                SaveTextureToFile(texture, "downloadedTexture.png");
+
+                Debug.Log("Texture downloaded and saved successfully.");
+            }
+            else
+            {
+                Debug.Log("Failed to download texture. Error: " + www.error);
+            }
+        }
+    }
+
+    private void SaveTextureToFile(Texture2D texture, string filename)
+    {
+        byte[] bytes = texture.EncodeToPNG();
+        string filePath = Path.Combine(Application.persistentDataPath, filename);
+        File.WriteAllBytes(filePath, bytes);
     }
 }
+
